0d8b1e4936358195cf8cd323707b0f47c0e36615
João Madeira <joaomadeira@outlook.pt>
2020-08-06 00:00:46 +0100

    Pageheader fixed

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -21,9 +21,9 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
             <div className="header-content">
                 <strong>{props.title}</strong>
+                {props.children}
             </div>
         </header>
-
         
     )
 }
