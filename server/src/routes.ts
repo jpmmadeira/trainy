a5ba8b0a2915e2510a054691c0110d74599fdb20
João Madeira <joaomadeira@outlook.pt>
2020-08-10 16:51:00 +0100

    Server application finished

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import WorkoutsController from './controllers/WorkoutsController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
+
 const workoutsController = new WorkoutsController();
+const connectionsController = new ConnectionsController();
 
 routes.get('/workouts', workoutsController.index);
 routes.post('/workouts', workoutsController.create);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
